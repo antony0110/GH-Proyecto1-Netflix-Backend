23710cf07338e963492741c19c8e7e4a39a6f799
Antony Gomez <60438018+antony1010@users.noreply.github.com>
2020-04-20 21:34:25 +0200

    Login y Regsiter

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-fsdfbsdbkfbksdfbksdbf
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const userRoutes = require('./routes/users')
+
+app.use(express.json());
+app.use('/users',userRoutes);
+
+app.listen(PORT,()=> console.log('server running on port' + PORT));
