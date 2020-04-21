1c750c17adb419e918006eedf0cc8f444ae1b4d5
Antony Gomez <60438018+antony1010@users.noreply.github.com>
2020-04-21 22:33:03 +0200

    Token

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -1,5 +1,6 @@
-const { User } = require('../models/index')
+const { User,Token } = require('../models/index')
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 const UserController={
     
    async register(req,res){
@@ -28,12 +29,14 @@ const UserController={
             if(!isMatch){
                 return res.status(400).send({message:"Usuario o contraseña incorrectos"})
             } 
-            res.send(user)
+            const token = jwt.sign({id:user.id}, 'mimamaMeMima' , {expiresIn:'2w'} );
+            Token.create({token,UserId:user.id});//añadimos el token al final del array
+            res.send({message:'Bienvenido Sr', user,token});
           });
         })
     },
     Profiles(req,res){
-            User.findAll()
+            User.findAll({attributes: { exclude: ['password'] }})
             .then(users => res.send(users))
             .catch(err=>{
                 console.log(err)
@@ -42,3 +45,34 @@ const UserController={
         }
     };
 module.exports=UserController;
+
+
+//async login(req, res) {
+  //  try {
+    //    const user = await UserModel.findOne({ //buscamos el usuario por el email, ej: 'user@email.com'
+      //      email: req.body.email
+        ///});
+        //console.log(user)
+       // if(!user){
+       //    return res.status(400).send({message:'Wrong credentials'});
+     //   }
+        //const isMatch = await bcrypt.compare(req.body.password,user.password);
+      //  console.log(isMatch)
+    //    if(!isMatch){
+      //     return res.status(400).send({message:'Wrong credentials'});
+    //    }
+    //    const token = jwt.sign({"_id":"5e9ec482ebead138a00b8d32"}, 'mimamaMeMima' , {expiresIn:'2w'} );
+   //     if(user.tokens.length>4) user.tokens.shift();//si ya hay 5 tokens eliminamos el más antiguo
+     //   user.tokens.push(token);//añadimos el token al final del array
+   //     await user.save();//guarda los cambios en mongoDB
+      //  res.send({message:'Welcome Mr '+user.email,user,token});
+ //   } catch (error) {
+        //console.error(error)
+    //    res.status(500).send({
+        //    message: 'There was a problem trying to log in'
+     //   })
+  //  }
+//}
+//}
+
+//module.exports = UserController;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 const userRoutes = require('./routes/users')
 
 app.use(express.json());
